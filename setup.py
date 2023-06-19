f6488a13bdf4989d24350d9663fb0d17a42e35af
Adam Schubert <adam.schubert@sg1-game.net>
2023-06-19 04:51:35 +0200

    New version 0.1.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xlf-merge',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(exclude=['tests', 'tests.*']),
     package_data={'xlf_merge': ['py.typed']},
     install_requires=[
